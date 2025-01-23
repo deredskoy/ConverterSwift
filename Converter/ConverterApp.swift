54aec688d2d6423b171692a934ca65c101641f85
Vitaliy Mart <cryheme@gmail.com>
2025-01-23 16:16:08 +0200

    Initial commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Vitaliy Martynov on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
