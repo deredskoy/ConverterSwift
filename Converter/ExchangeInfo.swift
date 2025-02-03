a28aa46f2868d70dca58a6150870533784784897
Vitaliy Mart <cryheme@gmail.com>
2025-02-03 23:14:26 +0200

    info view added

---
+++ Converter/ExchangeInfo.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeInfo.swift
+//  Converter
+//
+//  Created by Vitaliy Martynov on 2/3/25.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
