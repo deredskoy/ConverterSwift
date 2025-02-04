cd5abe1fa6f057298941ffa284c2e060e039a794
Vitaliy Mart <cryheme@gmail.com>
2025-02-04 21:57:32 +0200

    Exhcnage rates view button added

--- Converter/ExchangeInfo.swift
+++ Converter/ExchangeInfo.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct ExchangeInfo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // Background color
+            Color(red: 1/255, green: 121/255, blue: 111/255)
+                .ignoresSafeArea()
+            VStack {
+                // Title text
+                Text("Exchange rates")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                // Description text
+                Text("Select the currency you want to exchange")
+                    .font(.title3)
+                    .foregroundColor(.white)
+                // Exchange rates
+                HStack {
+                    // Left currency
+                    
+                    // Exchange rate text
+                    
+                    // Right currency
+                }
+                
+                // Done button
+                Button("Done") {
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.gray.mix(with: .black, by: 0.5))
+                .font(.title3)
+                .padding()
+            }
+        }
     }
 }
 
